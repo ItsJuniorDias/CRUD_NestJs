ee61b13cf0976d8331af559342921abba4268900
AlexandreJr <juniordias_@live.com>
2024-11-08 15:03:29 -0300

    chore: add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { UsersModule } from "./users/users.module";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      "mongodb+srv://reabilitado97:zs1oAU5swuN1NTlw@cluster0.gulwx.mongodb.net/"
+    ),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
