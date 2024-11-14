eb68a8e62bbbffc611bd4ffe4a692a6207abc796
AlexandreJr <juniordias_@live.com>
2024-11-13 23:52:03 -0300

    add custom files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from './auth/auth.module';
+import { Module } from "@nestjs/common";
+import { UsersModule } from "./users/users.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AuthModule } from "./auth/auth.module";
+require("dotenv").config();
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://juniordias:gNPxdfPGyF7A37XJ@cluster0.b7w3c.mongodb.net/',
+      "mongodb+srv://juniordias:gNPxdfPGyF7A37XJ@cluster0.b7w3c.mongodb.net/"
     ),
     UsersModule,
     AuthModule,
