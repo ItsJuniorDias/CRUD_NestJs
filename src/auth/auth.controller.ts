56057c4df982dd147bdfa50104ca5a1280b45d31
AlexandreJr <juniordias_@live.com>
2024-11-15 15:12:54 -0300

    chore: add auth guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,22 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthDto } from './dtos/auth-user.dto';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from "@nestjs/common";
+import { AuthGuard } from "./auth.guard";
+import { AuthService } from "./auth.service";
+import { AuthDto } from "./dtos/auth-user.dto";
+import { SetMetadata } from "@nestjs/common";
 
-@Controller('auth')
+export const IS_PUBLIC_KEY = "isPublic";
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
+@Controller("auth")
 export class AuthController {
   constructor(private authService: AuthService) {}
 
@@ -10,5 +24,17 @@ export class AuthController {
   @Post()
   signIn(@Body() signInDto: AuthDto) {
     return this.authService.signIn(signInDto.email, signInDto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get("profile")
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @Public()
+  @Get()
+  findAll() {
+    return [];
   }
 }
