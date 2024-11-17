55075268e00666c28e5ad952ed0c97f8030fdb3b
AlexandreJr <juniordias_@live.com>
2024-11-17 12:45:22 -0300

    chore: add testing controller users

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -14,6 +14,10 @@ describe("UserController", () => {
           provide: UsersService,
           useValue: {
             findAll: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
           },
         },
       ],
@@ -28,6 +32,42 @@ describe("UserController", () => {
 
     usersService.findAll();
 
+    console.log(usersService.findAll(), "FIND ALL");
+
     expect(usersService.findAll).toHaveBeenCalled();
+  });
+
+  it("should call function findOne", async () => {
+    const id = "1234567";
+    usersService.findOne(id);
+
+    expect(usersService.findOne).toHaveBeenCalled();
+  });
+
+  it("should call function create", async () => {
+    usersService.create({
+      email: "milenamartim@live.com",
+      name: "Milena Martim",
+      password: "123467",
+    });
+
+    expect(usersService.create).toHaveBeenCalled();
+  });
+
+  it("should call function update", async () => {
+    const id = "1234567";
+    usersService.update(id, {
+      email: "juniordias@gmail.com.br",
+      name: "Alexandre",
+    });
+
+    expect(usersService.update).toHaveBeenCalled();
+  });
+
+  it("should call function remove", async () => {
+    const id = "1234567";
+    usersService.remove(id);
+
+    expect(usersService.remove).toHaveBeenCalled();
   });
 });
